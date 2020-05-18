8603a9b32c0695167fcd688a05737c5409b55d28
Kelvin Jimenez <kelvin@kelvin.cr>
2020-05-18 21:18:11 +0700

    implement kafka-server

--- kafka_server.py
+++ kafka_server.py
@@ -1,16 +1,16 @@
 import producer_server
 
+BROKER_URL = "PLAINTEXT://localhost:9092,PLAINTEXT://localhost:9093,PLAINTEXT://localhost:9094"
+
 
 def run_kafka_server():
-	# TODO get the json file path
-    input_file = ""
-
-    # TODO fill in blanks
+    input_file = "police-department-calls-for-service.json"
+    topic_name = "org.sanfranciscopolice.stats.calls"
     producer = producer_server.ProducerServer(
         input_file=input_file,
-        topic="",
-        bootstrap_servers="",
-        client_id=""
+        topic=topic_name,
+        bootstrap_servers=f"{BROKER_URL}",
+        client_id=f"producer.{topic_name}"
     )
 
     return producer
