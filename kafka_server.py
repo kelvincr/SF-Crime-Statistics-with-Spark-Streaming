9f058d6a7563dd01fc67a7969a36044aa4de2cab
Kelvin Jimenez <kelvin@kelvin.cr>
2020-05-19 00:03:05 +0700

    feat: kafka server implementation This is a kafka-consumer that read calls records from the SF police deparment and the push to a kafka-topic

--- kafka_server.py
+++ kafka_server.py
@@ -1,24 +1,39 @@
+import logging
+import logging.config
+from pathlib import Path
+
+logging.config.fileConfig(f"{Path(__file__).parents[0]}/logging.ini")
 import producer_server
 
-BROKER_URL = "PLAINTEXT://localhost:9092,PLAINTEXT://localhost:9093,PLAINTEXT://localhost:9094"
+BROKER_URL = "localhost:9092"
+
+logger = logging.getLogger(__name__)
 
 
 def run_kafka_server():
     input_file = "police-department-calls-for-service.json"
-    topic_name = "org.sanfranciscopolice.stats.calls"
+    calls_topic_name = "org.sanfranciscopolice.stats.calls"
+
     producer = producer_server.ProducerServer(
         input_file=input_file,
-        topic=topic_name,
+        topic=calls_topic_name,
+        num_partitions=1,
+        num_replicas=1,
         bootstrap_servers=f"{BROKER_URL}",
-        client_id=f"producer.{topic_name}"
+        client_id=f"producer.{calls_topic_name}",
     )
 
     return producer
 
 
 def feed():
-    producer = run_kafka_server()
-    producer.generate_data()
+    try:
+        producer = run_kafka_server()
+        producer.generate_data()
+    except KeyboardInterrupt as e:
+        logger.info("Shutting down")
+        producer.flush()
+        producer.close(5)
 
 
 if __name__ == "__main__":
--- producer_server.py
+++ producer_server.py
@@ -1,25 +1,62 @@
 from kafka import KafkaProducer
+from confluent_kafka.admin import AdminClient, NewTopic
+import logging
 import json
 import time
+import datetime
+
+logger = logging.getLogger(__name__)
+logging.getLogger("kafka.conn").setLevel('ERROR')
 
 
 class ProducerServer(KafkaProducer):
 
-    def __init__(self, input_file, topic, **kwargs):
+    def __init__(self, input_file, topic, num_partitions, num_replicas, **kwargs):
         super().__init__(**kwargs)
         self.input_file = input_file
         self.topic = topic
+        self.num_partitions = num_partitions
+        self.num_replicas = num_replicas
+        client = AdminClient({"bootstrap.servers": kwargs.get('bootstrap_servers')})
+        exists = self.topic_exists(client)
+        if exists is False:
+            self.create_topic(client, self.topic)
 
-    #TODO we're generating a dummy data
+    def read_file(self):
+        """
+        Read json file of calls.
+        """
+        with open(self.input_file, 'r') as f:
+            data = json.load(f)
+        return data
+
     def generate_data(self):
-        with open(self.input_file) as f:
-            for line in f:
-                message = self.dict_to_binary(line)
-                # TODO send the correct data
-                self.send()
-                time.sleep(1)
+        records = self.read_file()
+        logger.info(f"started producing data for kafka topic {self.topic}")
+        for i in records:
+            message = self.dict_to_binary(i)
+            self.send(self.topic, message)
+            curr_time = datetime.datetime.utcnow().replace(microsecond=0)
+            logger.info(f"{len(message)} bytes sent at {curr_time.isoformat()}")
+            time.sleep(1)
 
-    # TODO fill this in to return the json dictionary to binary
-    def dict_to_binary(self, json_dict):
-        return 
-        
+    @staticmethod
+    def dict_to_binary(json_dict):
+        return json.dumps(json_dict).encode("utf-8")
+
+    def topic_exists(self, client):
+        """Checks if the given topic exists"""
+        topic_metadata = client.list_topics(timeout=5)
+        return topic_metadata.topics.get(self.topic) is not None
+
+    def create_topic(self, client, topic_name):
+        """Creates the producer topic"""
+        futures = client.create_topics(
+            [NewTopic(topic=topic_name, num_partitions=self.num_partitions, replication_factor=self.num_replicas)]
+        )
+        for _, future in futures.items():
+            try:
+                future.result()
+                logger.info(f"kafka topic {self.topic} created")
+            except Exception as e:
+                logger.info(f"topic creation kafka failed {self.topic} with exception: {e}")
