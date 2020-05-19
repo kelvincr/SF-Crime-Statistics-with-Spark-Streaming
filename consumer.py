5dcc3db569424bcfececaad1ef27677d3514bad8
Kelvin Jimenez <kelvin@kelvin.cr>
2020-05-19 22:11:14 +0700

    feat: Kafka Consumer
    This is a sample of a kafka consumer using confluent_kafka library to read messages from `org.sanfranciscopolice.stats.calls`

---
+++ consumer.py
@@ -0,0 +1,28 @@
+import time
+from confluent_kafka import Consumer
+
+BROKER_URL = "PLAINTEXT://localhost:9092"
+
+
+def consume(topic_name, earliest=True):
+    """Consumes data from the Kafka Topic"""
+    offset = "earliest" if earliest else "latest"
+    c = Consumer({"bootstrap.servers": BROKER_URL, "group.id": "0", "auto.offset.reset": offset})
+    c.subscribe([topic_name])
+
+    while True:
+        messages = c.consume(5, timeout=1.0)
+        for message in messages:
+            print(f"consume message {message.key()}: {message.value()}")
+        time.sleep(1)
+
+
+def main():
+    try:
+        consume("org.sanfranciscopolice.stats.calls")
+    except KeyboardInterrupt as e:
+        print("shutting down")
+
+
+if __name__ == "__main__":
+    main()
