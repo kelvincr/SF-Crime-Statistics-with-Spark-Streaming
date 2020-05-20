7ddf35f314cec0bea7ef51487351f3028e516a93
Kelvin Jimenez <kelvin@kelvin.cr>
2020-05-21 03:19:07 +0700

    feat: data structure streaming with pyspark
    This is a small sample of data streaming, using as input a kafka topic, and then aggregation of data using pyspark, using as sink the console. for the watermark of the input, we are using 30s and a window time of 60min. Also you can find some examples of the expected output in the README.md file. In order to make it work, we need to run our previous kafka_server.

--- README.md
+++ README.md
@@ -37,3 +37,25 @@ kafka-console-consumer --bootstrap-server localhost:9092 --topic org.sanfrancisc
 You will see an output like this
 
 ![kakfa console consumer](images/producer_output_with_kakfa-console-consumer.png)
+
+# Submit spark job and check results
+```
+spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.4 --master local[*] data_stream.py
+```
+You will see a lot of information, however we are interested in check if we are consuming and aggregating the data from Kafka.
+In order to check if 
+![batch_ingestion_aggregation](images/batch_ingestion_aggregation.png)
+
+Also after each new result from the query, you will also get a progress reporter, like the following,
+containing useful information about the performance of your spark application.
+![spark_progress_report](images/spark_progress_reporter.png)
+
+Finally, the Spark UI, is available in the port 3000, it can be configure in the spark session.
+```python
+    sparkSession = SparkSession \
+        .builder \
+        .config('spark.ui.port', 3000) 
+```
+It is a useful tool, to explore your Spark cluster, including running jobs, executors, queries.
+![spark_ui](images/spark_ui.png)
+
--- data_stream.py
+++ data_stream.py
@@ -1,60 +1,83 @@
 import logging
-import json
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 import pyspark.sql.functions as psf
 
-
-# TODO Create a schema for incoming resources
 schema = StructType([
+    StructField("crime_id", StringType(), True),
+    StructField("original_crime_type_name", StringType(), True),
+    StructField("report_date", StringType(), True),
+    StructField("call_date", StringType(), True),
+    StructField("offense_date", StringType(), True),
+    StructField("call_time", StringType(), True),
+    StructField("call_date_time", TimestampType(), True),
+    StructField("disposition", StringType(), True),
+    StructField("address", StringType(), True),
+    StructField("city", StringType(), True),
+    StructField("state", StringType(), True),
+    StructField("agency_id", StringType(), True),
+    StructField("address_type", StringType(), True),
+    StructField("common_location", StringType(), True)
 ])
 
-def run_spark_job(spark):
 
-    # TODO Create Spark Configuration
-    # Create Spark configurations with max offset of 200 per trigger
-    # set up correct bootstrap server and port
+def run_spark_job(spark):
     df = spark \
         .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("subscribe", "org.sanfranciscopolice.stats.calls") \
+        .option("startingOffsets", "earliest") \
+        .option("maxOffsetPerTrigger", 200) \
+        .option("maxRatePerPartition", 200) \
+        .option("stopGracefullyOnShutdown", "true") \
+        .load()
 
     # Show schema for the incoming resources for checks
     df.printSchema()
 
-    # TODO extract the correct column from the kafka input resources
     # Take only value and convert it to String
-    kafka_df = df.selectExpr("")
+    kafka_df = df.selectExpr("CAST(value AS STRING)", "timestamp")
 
-    service_table = kafka_df\
-        .select(psf.from_json(psf.col('value'), schema).alias("DF"))\
-        .select("DF.*")
+    service_table = kafka_df \
+        .select(psf.from_json(psf.col('value'), schema).alias("SERVICE_TABLE"), psf.col('timestamp')) \
+        .select("SERVICE_TABLE.*", "timestamp")
 
-    # TODO select original_crime_type_name and disposition
-    distinct_table = 
+    # select original_crime_type_name and disposition
+    distinct_table = service_table \
+        .select(
+        psf.col("original_crime_type_name"),
+        psf.col("disposition"),
+        psf.col("call_date_time"),
+        psf.col("timestamp")
+    ) \
+        .withWatermark("call_date_time", "30 seconds")
+
+    distinct_table.printSchema()
 
     # count the number of original crime type
-    agg_df = 
+    agg_df = distinct_table.groupBy("original_crime_type_name", psf.window("call_date_time", "60 minutes")).count()
 
-    # TODO Q1. Submit a screen shot of a batch ingestion of the aggregation
-    # TODO write output stream
+    # batch ingestion of the aggregation
+    # write output stream
     query = agg_df \
+        .writeStream \
+        .format("console") \
+        .outputMode("complete") \
+        .start()
 
-
-    # TODO attach a ProgressReporter
     query.awaitTermination()
 
-    # TODO get the right radio code json path
-    radio_code_json_filepath = ""
-    radio_code_df = spark.read.json(radio_code_json_filepath)
-
-    # clean up your data so that the column names match on radio_code_df and agg_df
-    # we will want to join on the disposition code
-
-    # TODO rename disposition_code column to disposition
+    # rename disposition_code column to disposition
     radio_code_df = radio_code_df.withColumnRenamed("disposition_code", "disposition")
 
-    # TODO join on disposition column
-    join_query = agg_df.
-
+    # join on disposition column
+    join_query = agg_df \
+        .join(radio_code_df, "disposition") \
+        .writeStream \
+        .format("console") \
+        .queryName("join") \
+        .start()
 
     join_query.awaitTermination()
 
@@ -62,15 +85,14 @@ def run_spark_job(spark):
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
 
-    # TODO Create Spark in Standalone mode
-    spark = SparkSession \
+    # Create Spark in Standalone mode
+    sparkSession = SparkSession \
         .builder \
+        .config('spark.ui.port', 3000) \
         .master("local[*]") \
         .appName("KafkaSparkStructuredStreaming") \
         .getOrCreate()
-
     logger.info("Spark started")
+    run_spark_job(sparkSession)
 
-    run_spark_job(spark)
-
-    spark.stop()
+    sparkSession.stop()
